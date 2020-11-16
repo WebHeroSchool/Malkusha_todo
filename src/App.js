61d8a7004934fad0d651a11ee493d7f898acb44f
Nastya <malkusha@mail.ru>
2020-11-16 19:29:07 +0300

    task

--- src/App.js
+++ src/App.js
@@ -2,15 +2,30 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const text = <a href=''>привет</a>;
+  const autumn = true;
+  let month = 11;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello!
+        <h1 style={{
+          color:'#DF8D98'
+        }}>
+          Всем {text}!
         </h1>
+        <h3>
+          Заканчивается {2020} год.
+        </h3>
         <p>
-          There are some changes in <code>src/App.js</code>.
+          Новый год через {31 + 15} дней.
+        </p>
+        <p>
+          Сейчас {autumn ? 'осень' : 'не осень'}. Месяц {month && 'ноябрь'}.
+          {null}
+          {undefined}
+          {true}
+          {false}
         </p>
         <a
           className="App-link"
