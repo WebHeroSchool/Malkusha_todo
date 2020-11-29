9ba6a8f88dccc86439811019cab65addb33514bc
Nastya <malkusha@mail.ru>
2020-11-29 16:19:16 +0300

    material ui

--- src/Components/InputItem/InputItem.js
+++ src/Components/InputItem/InputItem.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const InputItem = () => (
   <div>
-    InputItem
+  <TextField
+          id="outlined-full-width"
+          style={{ margin: 8 }}
+          placeholder="Add a task"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+        />
   </div>);
 
 export default InputItem;
