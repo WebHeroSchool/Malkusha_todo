4de82b03504ddd4236324009318825ec13c06c69
Nastya <malkusha@mail.ru>
2020-12-03 12:11:18 +0300

    component-class

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,34 +6,36 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-    {
-      value: 'To finish studying',
-      isDone: false,
-    },
-    {
-      value: 'To decorate a Christmas tree',
-      isDone: false,
-    },
-    {
-      value: 'To make "olivye" salad',
-      isDone: true,
-    }
-  ];
+class App extends React.Component {
+  render () {
+    const items = [
+      {
+        value: 'To finish studying',
+        isDone: false,
+      },
+      {
+        value: 'To decorate a Christmas tree',
+        isDone: false,
+      },
+      {
+        value: 'To make "olivye" salad',
+        isDone: true,
+      }
+    ];
 
-  return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>&#9731; Christmas tasks:</h1>
-      <InputItem />
-      <ButtonGroup variant="text" size="small" color="secondary" aria-label="text primary button group">
-        <Button>All</Button>
-        <Button>Active</Button>
-        <Button>Completed</Button>
-      </ButtonGroup>
-      <ItemList items={items} />
-      <Footer count={3}/>
-    </div>);
-}
+    return (
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>&#9731; Christmas tasks:</h1>
+        <InputItem />
+        <ButtonGroup variant="text" size="small" color="secondary" aria-label="text primary button group">
+          <Button>All</Button>
+          <Button>Active</Button>
+          <Button>Completed</Button>
+        </ButtonGroup>
+        <ItemList items={items} />
+        <Footer count={3}/>
+      </div>);
+  }
+};
 
 export default App;
