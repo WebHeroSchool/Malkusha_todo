70b10d4def5b2d70a05e7badcc703c14cd286658
Nastya <malkusha@mail.ru>
2020-12-08 15:22:59 +0300

    methods

--- src/Components/Item/Items.js
+++ src/Components/Item/Items.js
@@ -5,28 +5,42 @@ import styles from './Item.module.css';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 
+class Item extends React.Component {
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
 
-const Item = ({ value, id, isDone, onClickDone, onClickDelete}) => (
-  <span>
-    <span className={classnames({
-    [styles.item]: true,
-    [styles.done]: isDone
-    })
-  } onClick = {() => onClickDone(id)}>
-      {value}
-    </span>
-    <IconButton aria-label="delete">
-      <DeleteIcon onClick = {() => onClickDelete(id)} />
-    </IconButton>
-  </span>
-)
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  render() {
+    const { value, id, isDone, onClickDone, onClickDelete} = this.props;
+    return (
+      <span>
+        <span className={classnames({
+        [styles.item]: true,
+        [styles.done]: isDone
+        })
+      } onClick = {() => onClickDone(id)}>
+          {value}
+        </span>
+        <IconButton aria-label="delete">
+          <DeleteIcon onClick = {() => onClickDelete(id)} />
+        </IconButton>
+      </span>
+    )
+  }
+}
 
 Item.defaultProps = {
     isDone: false
 };
 
-Item.PropTypes = {
-  value: PropTypes.string.isRequired
-};
+//Item.PropTypes = {  value: PropTypes.string.isRequired};
 
 export default Item;
